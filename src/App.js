e4cfb51aec514d8a97337e3ac46d97b2d3bf5273
Michael Dean <contact@michaeldean.ca>
2021-05-26 15:15:51 +0100

    first checkbox and image fade animation for when an image has been clicked

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 
 /* for preloading */
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 /* Load images from a folder for all the cats */
 const catchaCatImages = collectImages(require.context('./images/cats', false, /\.(png|jpe?g|svg)$/));
@@ -9,6 +9,9 @@ const catchaIcons = collectImages(require.context('./images/icons', false, /\.(p
 
 //console.log(JSON.stringify(catchaCatImages, null, 2));
 
+/*
+ * Render the webpage with the app
+ */
 function App() {
   return (
     <div className="App">
@@ -26,7 +29,7 @@ function App() {
               </div>
 
             {/* Grid of images */}
-              <CatchaImages />
+              <CatchaImageGrid />
 
               {/* Bottom of catcha */}
               <div class="catcha-footer">
@@ -55,12 +58,16 @@ function App() {
 }
 
 
-  
+/* 
+ * When the verify button is clicked, show a new set of random images.
+ */  
 function refreshPage() {
   window.location.reload(false);
 }
 
-/* Load images from a folder */
+/* 
+ * Load images from a folder
+ */
 function collectImages(r) {
 
   let images = {};
@@ -69,8 +76,12 @@ function collectImages(r) {
 
 }
 
-/* Component to show a grid of images */
-function CatchaImages() {
+/* 
+ * ===================================
+ * Component to show a grid of random images, without repeating an image.
+ * ===================================
+ */
+function CatchaImageGrid() {
 
   var gridSize = 9;
   var gridImages = [];
@@ -81,10 +92,12 @@ function CatchaImages() {
   //stores random numbers used to choose images randomly from the folder
   var imageNumbers = [];
 
-  /* Create the <div> and insert a random image on the image grid */
-  for(let i=1; i <= gridSize; i++){
+  /* 
+   * Create the <div> and insert a random image on the image grid
+   */
+  for(let imageIndex=1; imageIndex <= gridSize; imageIndex++){
 
-    /* Generate a random numbers. Generate a new one if it already has been chosen. */
+    /* Generate a random number. Generate a new one if it already has been chosen. */
     var num = randomNumber(numberOfImages);
 
     //Has the random number already been chosen? If it has, choose another number
@@ -95,19 +108,22 @@ function CatchaImages() {
     //Add random number to the array
     imageNumbers.push(num);
 
-    //format it to be exactly 2 digits (ex. 01, 11, 04 etc) for the filenames
-    let filenumber = String(num).padStart(2, '0');       
+    /*
+     * Filenames are written like "cat_01.jpg"
+     * So format filenumbers to be exactly 2 digits (ex. 01, 11, 04 etc) for the filenames
+     */
+    let fileNumber = String(num).padStart(2, '0');       
     
-    //Create the <div> and insert a random image on the image grid
+    /*
+     * Create the <div> and insert a random image on the image grid;
+     */
     gridImages.push(
-      <div id={"image" + i}>
-        <img src={catchaCatImages["cats_" + filenumber + ".jpg"].default} />
-      </div>
+        <CatchaImage fileNumber={fileNumber} imageIndex={imageIndex}/>
     );
   }
 
   /* 
-   * Preload the images
+   * Preload the images. @todo this doesn't seem to work
    */
   useEffect(() => {
 
@@ -119,10 +135,43 @@ function CatchaImages() {
   return <div className="catcha-images">{gridImages}</div>;
 }
 
-/* Generate a random number */
+/* 
+ * Generate a random number
+ */
 function randomNumber(maxNumber) {
   var randomNumber = Math.floor((Math.random() * maxNumber) + 1);
   return randomNumber;
+}
+
+/*
+ * =================================== 
+ * Component for a single CATCHA image.
+ * ===================================
+ */
+function CatchaImage(props) {
+
+  /*
+   * State variables
+   * isSelected: so we know if the image has been clicked or not. Not clicked by default.
+   */
+  const [isSelected, setSelected] = useState(false);
+
+  /*
+   * Add a class "ci-clicked" if the image has been clicked to resize the image.
+   * Also show
+   * The <span> element is just used to vertically align the image (both set to inline-block).
+   */
+  return (
+      <div id={"image" + props.imageIndex} className="catcha-single-image">
+        <div className={isSelected ? "checkmark image-is-clicked" : "checkmark" }></div>
+        <span className="catcha-image-size-aligner"></span>
+        <img 
+            src={catchaCatImages["cats_" + props.fileNumber + ".jpg"].default} 
+            className={isSelected ? "catcha-image image-is-clicked" : "catcha-image" } 
+            onClick={() => setSelected(!isSelected )}
+          />
+      </div>
+      )
 }
 
 export default App;
