e764d74f72460e807aea91453d7afeaaed118bd5
Michael Dean <contact@michaeldean.ca>
2021-05-28 17:57:50 +0100

    added cars and cats switching functionality on refresh

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 
 /* Load images from a folder for all the cats */
 const catchaCatImages = collectImages(require.context('./images/cats', false, /\.(png|jpe?g|svg)$/));
+const catchaCarImages = collectImages(require.context('./images/cars', false, /\.(png|jpe?g|svg)$/));
 const catchaIcons = collectImages(require.context('./images/icons', false, /\.(png|jpe?g|svg)$/));
 
 //console.log(JSON.stringify(catchaCatImages, null, 2));
@@ -13,6 +14,34 @@ const catchaIcons = collectImages(require.context('./images/icons', false, /\.(p
  * Render the webpage with the app
  */
 function App() {
+
+  /* 
+   * Store which image type has been used for the CATCAH (cars or cats)
+   * 0 - is for cats
+   * 1 - is for cars
+   */
+
+  const [imageType, setImageType] = useState(0);
+
+  //this effect runs first so it doesn't get overwritten
+  useEffect(() => {
+    const data = localStorage.getItem('localData');
+
+    //first check if there is already data, then parse it.
+    if(data) {
+
+      //set the image type to the opposite of the last time
+      setImageType(!JSON.parse(data));
+    }
+  }, []); // Only run after the first render
+
+  //store image type in local storage
+  useEffect(() => {
+    localStorage.setItem("localData", JSON.stringify(imageType));
+  });
+
+  console.log(imageType);
+
   return (
     <div className="App">
 
@@ -23,15 +52,17 @@ function App() {
 
               <div className="catcha-top-elements-container">
 
-            {/* Header */}
+                {/* Header */}
                 <div className="catcha-header">
                   <p>Select all images with</p>
-                  <h2>cats</h2>
+                  {/* If imageType is 0, show cats, otherwise, show cars. */}
+                  <h2>{imageType ? "cars" : "cats"}</h2>
                   <p>Click verify once there are none left.</p>
                 </div>
 
-              {/* Grid of images */}
-                <CatchaImageGrid />
+                {/* Grid of images */}
+                {/* If imageType is 0, show cats, otherwise, show cars. */}
+                <CatchaImageGrid imageType={imageType} />
 
               </div> {/* end catcha-top-elements-container */}
 
@@ -39,12 +70,13 @@ function App() {
               <div className="catcha-footer">
                 <span className="icon-size-aligner"></span>
                 <div className="catcha-icons">
-                  <div class="icon"><img src={catchaIcons['refresh_2x.png'].default} /></div>
-                  <div class="icon"><img src={catchaIcons['audio_2x.png'].default} /></div>
-                  <div class="icon"><img src={catchaIcons['info_2x.png'].default} /></div>
+                  <div className="icon"><img src={catchaIcons['refresh_2x.png'].default} /></div>
+                  <div className="icon"><img src={catchaIcons['audio_2x.png'].default} /></div>
+                  <div className="icon"><img src={catchaIcons['info_2x.png'].default} /></div>
                 </div>
                 <div className="button-container">
-                  <button className="verify-button" onClick={refreshPage}>VERIFY</button>
+                  {/* Before refreshing the page, change the image type for show the next type when the page refreshes  refreshPage() */}
+                  <button className="verify-button" onClick={() => {refreshPage()}}>VERIFY</button>
                 </div>
               </div>
 
@@ -54,8 +86,8 @@ function App() {
       </div>
         
 
-          <div className="image-placeholder">
-          </div>
+      <div className="image-placeholder">
+      </div>
 
     </div>
   );
@@ -81,20 +113,33 @@ function collectImages(r) {
 }
 
 /* 
- * ===================================
+ * ==============================================================
  * Component to show a grid of random images, without repeating an image.
- * ===================================
+ * ==============================================================
  */
-function CatchaImageGrid() {
+function CatchaImageGrid(props) {
 
-  var gridSize = 9;
-  var gridImages = [];
+  const gridSize = 9;
+  const gridImages = [];
+  const images = props.imageType ? catchaCarImages : catchaCatImages;
 
   //total number of images in assets folder
-  var numberOfImages = Object.keys(catchaCatImages).length;
+  const numberOfImages = Object.keys(images).length;
 
   //stores random numbers used to choose images randomly from the folder
-  var imageNumbers = [];
+  const imageNumbers = [];
+
+
+  /* 
+   * Preload the images. @todo this doesn't seem to work
+   */
+  // useEffect(() => {
+  //   const image = document.createElement("img");
+  //   image.src = catchaCatImages["cats_" + props.fileNumber + ".jpg"].default;
+  //   image.onload = () => {
+  //     setShow(true);
+  //   };
+  // }, []);
 
   /* 
    * Create the <div> and insert a random image on the image grid
@@ -102,7 +147,7 @@ function CatchaImageGrid() {
   for(let imageIndex=1; imageIndex <= gridSize; imageIndex++){
 
     /* Generate a random number. Generate a new one if it already has been chosen. */
-    var num = randomNumber(numberOfImages);
+    let num = randomNumber(numberOfImages);
 
     //Has the random number already been chosen? If it has, choose another number
     while(imageNumbers.includes(num)) {
@@ -116,25 +161,15 @@ function CatchaImageGrid() {
      * Filenames are written like "cat_01.jpg"
      * So format filenumbers to be exactly 2 digits (ex. 01, 11, 04 etc) for the filenames
      */
-    let fileNumber = String(num).padStart(2, '0');       
+    let fileNumber = String(num).padStart(2, '0');     
     
     /*
      * Create the <div> and insert a random image on the image grid;
      */
     gridImages.push(
-        <CatchaImage fileNumber={fileNumber} imageIndex={imageIndex}/>
+        <CatchaImage fileNumber={fileNumber} imageIndex={imageIndex} images={images} imageType={props.imageType} />
     );
   }
-
-  /* 
-   * Preload the images. @todo this doesn't seem to work
-   */
-  useEffect(() => {
-
-    gridImages.forEach(image => {
-      new Image().src = image;
-    })
-  });
 
   return <div className="catcha-images">{gridImages}</div>;
 }
@@ -159,10 +194,10 @@ function CatchaImage(props) {
    * isSelected: so we know if the image has been clicked or not. Not clicked by default.
    */
   const [isSelected, setSelected] = useState(false);
+  var fileName = (props.imageType ? "cars" : "cats") + "_" + props.fileNumber + ".jpg";
 
   /*
-   * Add a class "ci-clicked" if the image has been clicked to resize the image.
-   * Also show
+   * Add a class "image-is-clicked" if the image has been clicked to resize the image and show the checkmark.
    * The <span> element is just used to vertically align the image (both set to inline-block).
    */
   return (
@@ -170,7 +205,7 @@ function CatchaImage(props) {
         <div className={isSelected ? "checkmark image-is-clicked" : "checkmark" }></div>
         <span className="catcha-image-size-aligner"></span>
         <img 
-            src={catchaCatImages["cats_" + props.fileNumber + ".jpg"].default} 
+            src={props.images[fileName].default} 
             className={isSelected ? "catcha-image image-is-clicked" : "catcha-image" } 
             onClick={() => setSelected(!isSelected )}
           />
